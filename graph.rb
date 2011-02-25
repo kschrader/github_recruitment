538ddc2565547ce53ba6dd0264de916be779c336
Kurt Schrader <kschrader@gmail.com>
2011-02-25 14:33:45 -0500

    Remove the Java class that I couldn't get to load correctly in order to simplify extending this.

--- graph.rb
+++ graph.rb
@@ -3,7 +3,17 @@ require 'net/http'
 require 'yaml'
 require 'cgi'
 require 'bossman'
+include Java
 include BOSSMan
+
+jars = Dir['./lib/*.jar']
+jars.each do |jar| 
+  require jar
+end
+
+java_import Java::edu.uci.ics.jung.graph.DirectedSparseGraph
+java_import Java::edu.uci.ics.jung.algorithms.importance.BetweennessCentrality
+java_import Java::edu.uci.ics.jung.graph.util.EdgeType
 
 $stdout.sync = true
 puts "Getting URLs"
@@ -11,8 +21,9 @@ BOSSMan.application_id = YAML.load_file("config.yml")['yahookey']
 offset = 0
 done = false
 urls = []
+location = ARGV[0]
 while !done
-    results = BOSSMan::Search.web('site:github.com location "' + ARGV[0] + '" "profile - github"', :start => offset)
+    results = BOSSMan::Search.web('site:github.com location "' + location + '" "profile - github"', :start => offset)
     offset += results.count.to_i
     if offset > results.totalhits.to_i
         done = true
@@ -21,7 +32,7 @@ while !done
 end
 
 puts "Getting social graph"
-graph = {}
+network = {}
 Net::HTTP.start('github.com') { |http|
     for line in urls
         sleep 1
@@ -36,16 +47,42 @@ Net::HTTP.start('github.com') { |http|
         response = http.request(req)
         case response 
         when Net::HTTPSuccess
-            graph[me] = []
+            network[me] = []
             for link in YAML.load(response.body)["users"]
-                graph[me] << link
+                network[me] << link
             end
         else
             puts "non-200: #{me}"
         end
     end
 }
-result = Java::Ranker.new.rank(graph)
+
+puts 'Ranking results'
+
+network_graph = DirectedSparseGraph.new
+count = 0
+
+network.each do |node|
+  user = node[0]
+  follows = node[1]
+  follows.each do |followed_user|
+    network_graph.addEdge(count, user, followed_user, EdgeType::DIRECTED)
+    count = count + 1
+  end
+end
+
+ranker = BetweennessCentrality.new(network_graph)
+result = Hash.new
+
+ranker.setRemoveRankScoresOnFinalize(false)
+ranker.evaluate
+
+network_graph.getVertices.each do |vertice|
+  result[vertice] = ranker.getVertexRankScore(vertice)
+end
+
+ranker = Java::Ranker.new
+p ranker
 for user in result.sort_by { |user,score| score }
     puts "#{user[0]} = #{user[1]}"
 end
